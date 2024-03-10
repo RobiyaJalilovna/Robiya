672e890dcb30031b14a1e60d5818ee2e8f1c3744
RobiyaJalilovna <159040655+RobiyaJalilovna@users.noreply.github.com>
2024-03-10 08:58:02 -0700

    rest_api

--- book/views.py
+++ book/views.py
@@ -7,3 +7,17 @@ class BookListView(ListView):
     template_name = 'book_list_html'
 
 
+from rest_framework.authtoken.views import ObtainAuthToken
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+        })
+
